39efa250e5dcb4ee5554f339d6ccf640688daaa8
root <root@localhost.localdomain>
2016-03-17 00:23:56 +0300

    added mkdir to init.sh script

--- init.sh
+++ init.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+sudo mkdir public
+sudo mkdir uploads
+sudo mkdir public/css
+sudo mkdir public/js
+sudo mkdir public/img
 sudo apt-get update
 sudo apt-get install nginx
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
